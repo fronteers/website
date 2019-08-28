346d871da3842fea7fe0acee0639178f79eb3957
Iain van der Wiel <iain.vanderwiel@gmail.com>
2019-08-28 23:02:40 +0200

    Implement FolderCreator to create folders for languages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
+import { join } from 'path';
+
 import { ContentfulClient } from './contentful/client';
+import { FolderCreator } from './folders/creator';
 
 // Init ENV variables
 dotenv.config();
@@ -8,9 +11,14 @@ const { CONTENTFUL_SPACE, CONTENTFUL_TOKEN } = process.env;
 // Setup Contentful client
 const client = new ContentfulClient(CONTENTFUL_SPACE, CONTENTFUL_TOKEN);
 
+// Init FolderCreator
+const folderCreator = new FolderCreator(join(process.cwd(), 'content'));
+
 const fetchLocales = async () => {
   const locales = await client.getLocales();
-  console.log(locales);
+  const nonDefaultLocals = locales.items.filter(locale => locale.default === false);
+
+  nonDefaultLocals.forEach(locale => folderCreator.createFolder(locale.code));
 };
 
 fetchLocales();
