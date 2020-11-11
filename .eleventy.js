83b867fbdff3d5a946b32d325f1918429aab802c
Edwin Martin <edwin@bitstorm.org>
2020-11-11 21:40:58 +0100

    Create component structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,27 @@
+const glob = require("fast-glob");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./content/css/");
   eleventyConfig.addPassthroughCopy({ "content/fonts": "assets/fonts" });
   eleventyConfig.addPassthroughCopy({ "content/images": "assets/images" });
 
-  eleventyConfig.addShortcode("tag", (tag) => `<div class="tag">${tag}</div>`);
+  // eleventyConfig.addShortcode("tag", (tag) => `<div class="tag">${tag}</div>`);
+
+  glob.sync("components/paired/**/*.js").forEach((file) => {
+    console.log("file", file);
+    let shortcodes = require("./" + file);
+    Object.keys(shortcodes).forEach((name) => {
+      eleventyConfig.addPairedShortcode(name, shortcodes[name]);
+    });
+  });
+
+  glob.sync("components/shortcodes/**/*.js").forEach((file) => {
+    console.log("file", file);
+    let shortcodes = require("./" + file);
+    Object.keys(shortcodes).forEach((name) => {
+      eleventyConfig.addShortcode(name, shortcodes[name]);
+    });
+  });
 
   return {
     dir: {
--- content/index.md
+++ content/index.md
@@ -6,5 +6,11 @@ lang: en
 
 ## And we do it with pride.
 
+Below are proof of concept components.
+
 {% tag "Online" %}
 {% tag "Marketingcommissie" %}
+
+{% block "Title" %}
+Dit is de content
+{% endblock %}
