255b774359f07c879cab5ce5bc6a2522c2284f31
Edwin Martin <edwin@bitstorm.org>
2020-11-11 20:53:21 +0100

    Move/delete generated files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./content/css/");
+  eleventyConfig.addPassthroughCopy({ "content/fonts": "assets/fonts" });
+  eleventyConfig.addPassthroughCopy({ "content/images": "assets/images" });
 
   eleventyConfig.addShortcode("tag", (tag) => `<div class="tag">${tag}</div>`);
 
