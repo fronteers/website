addc60eba7de7bdde18269311ac554f76cdcaeba
Edwin Martin <edwin@bitstorm.org>
2020-11-11 14:01:42 +0100

    Split css in seperate files

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
-module.exports = {
-  dir: {
-    input: "pages",
-    output: "dist",
-  },
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addWatchTarget("./content/css/");
+
+  return {
+    dir: {
+      input: "content",
+      output: "dist",
+    },
+  };
 };
