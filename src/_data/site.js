3c422012ebd1ac5ed38bac4aa90143cf9fd39be2
Anneke Sinnema <mail@annekesinnema.nl>
2021-07-10 18:32:32 +0200

    Improve language switch
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,20 @@
 module.exports = {
-  buildTime: new Date(),
-  baseUrl: "https://www.fronteers.nl",
-  name: "Fronteers",
-  twitter: "@fronteers",
+    buildTime: new Date(),
+    baseUrl: "https://www.fronteers.nl",
+    name: "Fronteers",
+    twitter: "@fronteers",
+    languages: [
+    {
+        label: "EN",
+        code: "en",
+        currentLabel: "Current language:",
+        switchLabel: "Schakel naar"
+    },
+    {
+        label: "NL",
+        code: "nl",
+        currentLabel: "Current language:",
+        switchLabel: "Switch to"
+    },
+  ],
 };
