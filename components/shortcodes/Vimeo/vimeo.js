56e5b7a2d2cc39e71e49c79a42f8df34b39c5907
Anneke Sinnema <mail@annekesinnema.nl>
2021-08-03 14:59:54 +0200

    Create Vimeo embed component
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

---
+++ components/shortcodes/Vimeo/vimeo.js
@@ -0,0 +1,1 @@
+exports.vimeo = (code) => `<iframe src="https://player.vimeo.com/video/${code}?texttrack=en" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
