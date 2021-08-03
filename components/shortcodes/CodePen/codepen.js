80b0d21135d6e7a288a8d63a98dac7260498b8b2
Anneke Sinnema <mail@annekesinnema.nl>
2021-08-03 14:59:25 +0200

    Create codepen embed component
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

---
+++ components/shortcodes/CodePen/codepen.js
@@ -0,0 +1,5 @@
+exports.codepen = (code, title) => `
+<iframe height="300" style="width: 100%;" scrolling="no" title="pool rules" src="https://codepen.io/kirstenallen/embed/${code}?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+  See <a href="https://codepen.io/kirstenallen/pen/${code}">
+  ${title}</a> on <a href="https://codepen.io">CodePen</a>.
+</iframe>`;
