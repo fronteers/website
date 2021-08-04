d018b747d37c7a7c58a6a5d4e1def1e01058ca3d
Anneke Sinnema <mail@annekesinnema.nl>
2021-08-04 12:41:09 +0200

    Added slides.com shortcode
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

---
+++ components/shortcodes/Slides/slides.js
@@ -0,0 +1,1 @@
+exports.slides = (user, code) => `<iframe src="https://slides.com/${user}/${code}/embed" width="576" height="420" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>`;
