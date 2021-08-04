203d09f6f3f957a9ee2a6d6100f3418b660428ce
Anneke Sinnema <mail@annekesinnema.nl>
2021-08-04 12:40:53 +0200

    Added mailchimp shortcode
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

---
+++ components/shortcodes/mailchimp/mailchimp.js
@@ -0,0 +1,9 @@
+exports.mailchimp = () => `<form action="https://fronteers.us10.list-manage.com/subscribe/post?u=d8d859a3bce138807ce785741&amp;id=49b991ad18" method="post" class="generated">
+    <fieldset>
+    <legend>Sign up for our newsletter</legend>
+    <p><label for="subscribe-email">E-mail address <em>required</em></label> <input type="email" name="EMAIL" id="subscribe-email" class="text"></p>
+    <p><label for="subscribe-voornaam">First name</label> <input type="text" name="FNAME" id="subscribe-voornaam" class="text"></p>
+    <p><label for="subscribe-achternaam">Last name</label> <input type="text" name="LNAME" id="subscribe-achternaam" class="text"></p>
+    <p class="submit"><input type="submit" value="Aanmelden" class="submit"></p>
+    </fieldset>
+</form>`;
