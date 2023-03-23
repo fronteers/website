648d71392c66c02eac5253c01982d8803898d814
Anneke Sinnema <mail@annekesinnema.nl>
2023-03-23 18:00:34 +0100

    Hide subpages for conference for now
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,306 +1,306 @@
 module.exports = {
-  header: {
-    nl: [
-      {
-        title: "Over Fronteers",
-        url: "/nl/vereniging",
-        subnav: [
-          {
-            title: "Organisatie",
-            url: "/nl/vereniging/organisatie",
-          },
-          {
-            title: "Bestuur",
-            url: "/nl/vereniging/bestuur",
-          },
-          {
-            title: "Algemene Ledenvergadering",
-            url: "/nl/vereniging/alv",
-          },
-          {
-            title: "Commissies",
-            url: "/nl/vereniging/commissies",
-          },
-          {
-            title: "Freelancers",
-            url: "/nl/freelancers",
-          },
-          {
-            title: "Support",
-            url: "/nl/vereniging/support",
-          },
-          {
-            title: "Geschiedenis",
-            url: "/nl/vereniging/geschiedenis",
-          },
+    header: {
+        nl: [
+            {
+                title: "Over Fronteers",
+                url: "/nl/vereniging",
+                subnav: [
+                    {
+                        title: "Organisatie",
+                        url: "/nl/vereniging/organisatie",
+                    },
+                    {
+                        title: "Bestuur",
+                        url: "/nl/vereniging/bestuur",
+                    },
+                    {
+                        title: "Algemene Ledenvergadering",
+                        url: "/nl/vereniging/alv",
+                    },
+                    {
+                        title: "Commissies",
+                        url: "/nl/vereniging/commissies",
+                    },
+                    {
+                        title: "Freelancers",
+                        url: "/nl/freelancers",
+                    },
+                    {
+                        title: "Support",
+                        url: "/nl/vereniging/support",
+                    },
+                    {
+                        title: "Geschiedenis",
+                        url: "/nl/vereniging/geschiedenis",
+                    },
+                ],
+            },
+            {
+                title: "Word lid",
+                url: "/nl/word-lid/",
+                subnav: [
+                    {
+                        url: "/nl/word-lid/lidmaatschap/",
+                        title: "Lidmaatschap",
+                    },
+                    {
+                        url: "/nl/word-lid/voorwaarden/",
+                        title: "Voorwaarden",
+                    },
+                    {
+                        url: "/nl/word-lid/code-of-conduct/",
+                        title: "Code of conduct",
+                    },
+                ],
+            },
+            {
+                url: "/nl/activiteiten/",
+                title: "Activiteiten",
+                subnav: [
+                    {
+                        url: "/nl/activiteiten/events/",
+                        title: "Meetups",
+                    },
+                    {
+                        url: "/nl/activiteiten/workshops/",
+                        title: "Workshops",
+                    },
+                ],
+            },
+            {
+                title: "Blog",
+                url: "/nl/blog/",
+            },
+            {
+                url: "/nl/leden/",
+                title: "Leden",
+            },
+            {
+                url: "/nl/werk-en-freelance/",
+                title: "Werk en freelance",
+                subnav: [
+                    {
+                        url: "/nl/werk-en-freelance/voorwaarden/",
+                        title: "Voorwaarden",
+                    },
+                    {
+                        url: "/nl/werk-en-freelance/vacature-plaatsen/",
+                        title: "Vacature plaatsen",
+                    },
+                ],
+            },
+            {
+                url: "/nl/congres/",
+                title: "Congres",
+                // subnav: [
+                //   {
+                //     url: "/nl/congres/tickets/",
+                //     title: "Tickets",
+                //   },
+                //   {
+                //     url: "/nl/congres/sprekers/",
+                //     title: "Sprekers",
+                //   },
+                //   {
+                //     url: "/nl/congres/programma/",
+                //     title: "Programma",
+                //   },
+                //   {
+                //     url: "/nl/congres/workshops/",
+                //     title: "Workshops",
+                //   },
+                //   {
+                //     url: "/nl/congres/lokaties/",
+                //     title: "Lokaties",
+                //   },
+                //   {
+                //     url: "/nl/congres/contact/",
+                //     title: "Contact",
+                //   },
+                //   {
+                //     url: "/nl/congres/2019/",
+                //     title: "Congres 2019",
+                //   },
+                //   {
+                //     url: "/nl/congres/2018/",
+                //     title: "Congres 2018",
+                //   },
+                // ],
+            },
         ],
-      },
-      {
-        title: "Word lid",
-        url: "/nl/word-lid/",
-        subnav: [
-          {
-            url: "/nl/word-lid/lidmaatschap/",
-            title: "Lidmaatschap",
-          },
-          {
-            url: "/nl/word-lid/voorwaarden/",
-            title: "Voorwaarden",
-          },
-          {
-            url: "/nl/word-lid/code-of-conduct/",
-            title: "Code of conduct",
-          },
+        en: [
+            {
+                title: "About Fronteers",
+                url: "/en/organisation",
+                subnav: [
+                    {
+                        title: "Organisation",
+                        url: "/en/organisation/organisation",
+                    },
+                    {
+                        title: "Board",
+                        url: "/en/organisation/board",
+                    },
+                    {
+                        title: "Membership meeting",
+                        url: "/en/organisation/members-meeting",
+                    },
+                    {
+                        title: "Committees",
+                        url: "/en/organisation/committees",
+                    },
+                    {
+                        title: "Freelancers",
+                        url: "/en/freelancers",
+                    },
+                    {
+                        title: "Support",
+                        url: "/en/organisation/support",
+                    },
+                    {
+                        title: "History",
+                        url: "/en/organisation/history",
+                    },
+                ],
+            },
+            {
+                title: "Join us",
+                url: "/en/join-us/",
+                subnav: [
+                    {
+                        url: "/en/join-us/membership/",
+                        title: "Membership",
+                    },
+                    {
+                        url: "/en/join-us/terms-and-conditions/",
+                        title: "Terms and conditions",
+                    },
+                    {
+                        url: "/en/join-us/code-of-conduct/",
+                        title: "Code of conduct",
+                    },
+                ],
+            },
+            {
+                url: "/en/activities/",
+                title: "Activities",
+                subnav: [
+                    {
+                        url: "/en/activities/events/",
+                        title: "Meetups",
+                    },
+                    {
+                        url: "/en/activities/workshops/",
+                        title: "Workshops",
+                    },
+                ],
+            },
+            {
+                title: "Blog",
+                url: "/en/blog/",
+            },
+            {
+                url: "/en/members/",
+                title: "Members",
+            },
+            {
+                url: "/en/jobs/",
+                title: "Jobs",
+                subnav: [
+                    {
+                        url: "/en/jobs/terms-and-conditions/",
+                        title: "Terms and conditions",
+                    },
+                    {
+                        url: "/en/jobs/post-job-opening/",
+                        title: "Post a job opening",
+                    },
+                ],
+            },
+            {
+                url: "/en/conference/",
+                title: "Conference",
+                // subnav: [
+                //   {
+                //     url: "/en/conference/tickets.html",
+                //     title: "Tickets",
+                //   },
+                //   {
+                //     url: "/en/conference/speakers.html",
+                //     title: "Speakers",
+                //   },
+                //   {
+                //     url: "/en/conference/schedule.html",
+                //     title: "Schedule",
+                //   },
+                //   {
+                //     url: "/en/conference/workshops.html",
+                //     title: "Workshops",
+                //   },
+                //   {
+                //     url: "/en/conference/venues.html",
+                //     title: "Venues",
+                //   },
+                //   {
+                //     url: "/en/conference/contact.html",
+                //     title: "Contact",
+                //   },
+                //   {
+                //     url: "/en/conference/2019/",
+                //     title: "Conference 2019",
+                //   },
+                //   {
+                //     url: "/en/conference/2018/",
+                //     title: "Conference 2018",
+                //   },
+                // ],
+            },
         ],
-      },
-      {
-        url: "/nl/activiteiten/",
-        title: "Activiteiten",
-        subnav: [
-          {
-            url: "/nl/activiteiten/events/",
-            title: "Meetups",
-          },
-          {
-            url: "/nl/activiteiten/workshops/",
-            title: "Workshops",
-          },
+    },
+    footer: {
+        nl: [
+            {
+                url: "/nl/footer/contact/",
+                title: "Contact",
+            },
+            {
+                url: "/nl/footer/nieuwsbrief/",
+                title: "Nieuwsbrief",
+            },
+            {
+                url: "/nl/footer/privacy-en-cookies/",
+                title: "Privacy &amp; cookie statement",
+            },
+            {
+                url: "/nl/footer/colofon/",
+                title: "Colofon",
+            },
+            {
+                url: "/nl/footer/adverteren/",
+                title: "Adverteren",
+            },
         ],
-      },
-      {
-        title: "Blog",
-        url: "/nl/blog/",
-      },
-      {
-        url: "/nl/leden/",
-        title: "Leden",
-      },
-      {
-        url: "/nl/werk-en-freelance/",
-        title: "Werk en freelance",
-        subnav: [
-          {
-            url: "/nl/werk-en-freelance/voorwaarden/",
-            title: "Voorwaarden",
-          },
-          {
-            url: "/nl/werk-en-freelance/vacature-plaatsen/",
-            title: "Vacature plaatsen",
-          },
+        en: [
+            {
+                url: "/en/footer/contact/",
+                title: "Contact",
+            },
+            {
+                url: "/en/footer/newsletter/",
+                title: "Newsletter",
+            },
+            {
+                url: "/en/footer/privacy-and-cookies/",
+                title: "Privacy &amp; cookie statement",
+            },
+            {
+                url: "/en/footer/colophon/",
+                title: "Colophon",
+            },
+            {
+                url: "/en/footer/advertise/",
+                title: "Advertise",
+            },
         ],
-      },
-      {
-        url: "/nl/congres/",
-        title: "Congres",
-        subnav: [
-          {
-            url: "/nl/congres/tickets/",
-            title: "Tickets",
-          },
-          {
-            url: "/nl/congres/sprekers/",
-            title: "Sprekers",
-          },
-          {
-            url: "/nl/congres/programma/",
-            title: "Programma",
-          },
-          {
-            url: "/nl/congres/workshops/",
-            title: "Workshops",
-          },
-          {
-            url: "/nl/congres/lokaties/",
-            title: "Lokaties",
-          },
-          {
-            url: "/nl/congres/contact/",
-            title: "Contact",
-          },
-          {
-            url: "/nl/congres/2019/",
-            title: "Congres 2019",
-          },
-          {
-            url: "/nl/congres/2018/",
-            title: "Congres 2018",
-          },
-        ],
-      },
-    ],
-    en: [
-      {
-        title: "About Fronteers",
-        url: "/en/organisation",
-        subnav: [
-          {
-            title: "Organisation",
-            url: "/en/organisation/organisation",
-          },
-          {
-            title: "Board",
-            url: "/en/organisation/board",
-          },
-          {
-            title: "Membership meeting",
-            url: "/en/organisation/members-meeting",
-          },
-          {
-            title: "Committees",
-            url: "/en/organisation/committees",
-          },
-          {
-            title: "Freelancers",
-            url: "/en/freelancers",
-          },
-          {
-            title: "Support",
-            url: "/en/organisation/support",
-          },
-          {
-            title: "History",
-            url: "/en/organisation/history",
-          },
-        ],
-      },
-      {
-        title: "Join us",
-        url: "/en/join-us/",
-        subnav: [
-          {
-            url: "/en/join-us/membership/",
-            title: "Membership",
-          },
-          {
-            url: "/en/join-us/terms-and-conditions/",
-            title: "Terms and conditions",
-          },
-          {
-            url: "/en/join-us/code-of-conduct/",
-            title: "Code of conduct",
-          },
-        ],
-      },
-      {
-        url: "/en/activities/",
-        title: "Activities",
-        subnav: [
-          {
-            url: "/en/activities/events/",
-            title: "Meetups",
-          },
-          {
-            url: "/en/activities/workshops/",
-            title: "Workshops",
-          },
-        ],
-      },
-      {
-        title: "Blog",
-        url: "/en/blog/",
-      },
-      {
-        url: "/en/members/",
-        title: "Members",
-      },
-      {
-        url: "/en/jobs/",
-        title: "Jobs",
-        subnav: [
-          {
-            url: "/en/jobs/terms-and-conditions/",
-            title: "Terms and conditions",
-          },
-          {
-            url: "/en/jobs/post-job-opening/",
-            title: "Post a job opening",
-          },
-        ],
-      },
-      {
-        url: "/en/conference/",
-        title: "Conference",
-        subnav: [
-          {
-            url: "/en/conference/tickets.html",
-            title: "Tickets",
-          },
-          {
-            url: "/en/conference/speakers.html",
-            title: "Speakers",
-          },
-          {
-            url: "/en/conference/schedule.html",
-            title: "Schedule",
-          },
-          {
-            url: "/en/conference/workshops.html",
-            title: "Workshops",
-          },
-          {
-            url: "/en/conference/venues.html",
-            title: "Venues",
-          },
-          {
-            url: "/en/conference/contact.html",
-            title: "Contact",
-          },
-          {
-            url: "/en/conference/2019/",
-            title: "Conference 2019",
-          },
-          {
-            url: "/en/conference/2018/",
-            title: "Conference 2018",
-          },
-        ],
-      },
-    ],
-  },
-  footer: {
-    nl: [
-      {
-        url: "/nl/footer/contact/",
-        title: "Contact",
-      },
-      {
-        url: "/nl/footer/nieuwsbrief/",
-        title: "Nieuwsbrief",
-      },
-      {
-        url: "/nl/footer/privacy-en-cookies/",
-        title: "Privacy &amp; cookie statement",
-      },
-      {
-        url: "/nl/footer/colofon/",
-        title: "Colofon",
-      },
-      {
-        url: "/nl/footer/adverteren/",
-        title: "Adverteren",
-      },
-    ],
-    en: [
-      {
-        url: "/en/footer/contact/",
-        title: "Contact",
-      },
-      {
-        url: "/en/footer/newsletter/",
-        title: "Newsletter",
-      },
-      {
-        url: "/en/footer/privacy-and-cookies/",
-        title: "Privacy &amp; cookie statement",
-      },
-      {
-        url: "/en/footer/colophon/",
-        title: "Colophon",
-      },
-      {
-        url: "/en/footer/advertise/",
-        title: "Advertise",
-      },
-    ],
-  },
+    },
 };
