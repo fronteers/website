41b6b7cb7428d5cc2ffac2ed7c948d57093f9262
Anneke Sinnema <mail@annekesinnema.nl>
2023-06-27 18:42:51 +0200

    Improve accessibility of language toggle
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,24 +1,26 @@
 module.exports = {
-  buildTime: new Date(),
-  baseUrl: "https://www.fronteers.nl",
-  name: "Fronteers",
-  twitter: "@fronteers",
-  languages: [
-    {
-      label: "NL",
-      code: "nl",
-      hreflang: "nl-NL",
-      currentLabel: "Current language:",
-      switchLabel: "Switch to",
-      baseUrl: "https://www.fronteers.nl",
-    },
-    {
-      label: "EN",
-      code: "en",
-      hreflang: "en-GB",
-      currentLabel: "Current language:",
-      switchLabel: "Schakel naar",
-      baseUrl: "https://www.fronteers.nl/en",
-    },
-  ],
+    buildTime: new Date(),
+    baseUrl: "https://www.fronteers.nl",
+    name: "Fronteers",
+    twitter: "@fronteers",
+    languages: [
+        {
+            label: "NL",
+            ariaLabel: "Nederlands",
+            code: "nl",
+            hreflang: "nl-NL",
+            currentLabel: "Huidige taal:",
+            switchLabel: "Schakel naar",
+            baseUrl: "https://www.fronteers.nl",
+        },
+        {
+            label: "EN",
+            ariaLabel: "English",
+            code: "en",
+            hreflang: "en-GB",
+            currentLabel: "Current language:",
+            switchLabel: "Switch to",
+            baseUrl: "https://www.fronteers.nl/en",
+        },
+    ],
 };
