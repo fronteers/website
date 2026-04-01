80607bd27535eabe83b4ad9943d8728e447cc455
Anneke Sinnema <mail@annekesinnema.nl>
2026-04-01 21:22:52 +0200

    Bring back job bank
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -88,28 +88,28 @@ module.exports = {
           title: "Blog",
           url: "/nl/blog/",
         },
-        // {
-        //   url: "/nl/werk-en-freelance/",
-        //   title: "Werk en freelance",
-        //   subnav: [
-        //     {
-        //       url: "/nl/werk-en-freelance/vacature-plaatsen/",
-        //       title: "Vacature plaatsen",
-        //     },
-        //     {
-        //       url: "/nl/werk-en-freelance/vacature-plaatsen/voorwaarden",
-        //       title: "Vacaturebank voorwaarden",
-        //     },
-        //     //   {
-        //     //     title: "Freelancers",
-        //     //     url: "/nl/freelancers",
-        //     //   },
-        //     {
-        //       title: "Mentors",
-        //       url: "/nl/mentors",
-        //     },
-        //   ],
-        // },
+        {
+          url: "/nl/werk-en-freelance/",
+          title: "Werk en freelance",
+          subnav: [
+            {
+              url: "/nl/werk-en-freelance/vacature-plaatsen/",
+              title: "Vacature plaatsen",
+            },
+            {
+              url: "/nl/werk-en-freelance/vacature-plaatsen/voorwaarden",
+              title: "Vacaturebank voorwaarden",
+            },
+            //   {
+            //     title: "Freelancers",
+            //     url: "/nl/freelancers",
+            //   },
+            {
+              title: "Mentors",
+              url: "/nl/mentors",
+            },
+          ],
+        },
         {
           url: "https://fronteersconf.org/",
           title: "Dark Mode",
@@ -253,28 +253,28 @@ module.exports = {
           title: "Blog",
           url: "/en/blog/",
         },
-        // {
-        //   url: "/en/jobs/",
-        //   title: "Jobs",
-        //   subnav: [
-        //     {
-        //       url: "/en/jobs/post-job-opening/",
-        //       title: "Post a job opening",
-        //     },
-        //     {
-        //       url: "/en/jobs/post-job-opening/toc",
-        //       title: "Job bank terms",
-        //     },
-        //     //   {
-        //     //     title: "Freelancers",
-        //     //     url: "/en/freelancers",
-        //     //   },
-        //     {
-        //       title: "Mentors",
-        //       url: "/en/mentors",
-        //     },
-        //   ],
-        // },
+        {
+          url: "/en/jobs/",
+          title: "Jobs",
+          subnav: [
+            {
+              url: "/en/jobs/post-job-opening/",
+              title: "Post a job opening",
+            },
+            {
+              url: "/en/jobs/post-job-opening/toc",
+              title: "Job bank terms",
+            },
+            //   {
+            //     title: "Freelancers",
+            //     url: "/en/freelancers",
+            //   },
+            {
+              title: "Mentors",
+              url: "/en/mentors",
+            },
+          ],
+        },
         {
           url: "https://fronteersconf.org/",
           title: "Dark Mode",
