867e45a6b026d110e2a56ed130db6643c5fc3ec7
Edwin Martin <edwin@bitstorm.org>
2026-05-23 14:43:45 +0200

    Use Chrome from puppeteer

--- .eleventy.js
+++ .eleventy.js
@@ -310,7 +310,11 @@ module.exports = async function (eleventyConfig) {
   if (!isDev) {
     eleventyConfig.on("afterBuild", async () => {
       async function convertSvgToJpeg(inputDir, outputDir) {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+          executablePath: await puppeteer.executablePath(),
+          args: ["--no-sandbox", "--disable-setuid-sandbox"],
+          headless: true,
+        });
         const page = await browser.newPage();
 
         // Read all files in the input directory
